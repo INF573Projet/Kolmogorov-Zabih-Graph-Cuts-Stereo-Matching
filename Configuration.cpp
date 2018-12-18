9c2198cf99aa6f5f042031ab8593c13b69a5a413
Leon Zheng <leonpapanoel@gmail.com>
2018-12-19 00:17:11 +0100

    Architecture of Configuration class

--- Configuration.cpp
+++ Configuration.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Configuration.h"
+
+Configuration::Configuration(const Mat &conf) {
+    /* conf is a Cubic matrix */
+    if(validConfMatrix()){
+        this->conf = conf;
+    } else {
+        throw std::invalid_argument("Configuration matrix isn't valid: need a 3D matrix with correct size");
+    }
+}
+
+Configuration::Configuration(const Mat &image, int minDisp, int maxDisp) {
+    /* Constructor with all inactive assignments, given an image and the disparity range */
+    int sizes[] = {image.rows, image.cols, maxDisp - minDisp + 1 };
+    Mat conf = Mat(3, sizes, CV_8U, Scalar(0));
+    this->conf = conf;
+}
+
+bool Configuration::validConfMatrix(){
+    /* Verify if the configuration matrix is a valid format */
+    return true;
+}
+
+Mat Configuration::computeDisparity(){
+    /* Compute the disparity given the assignments */
+    return Mat();
+}
+
--- Configuration.h
+++ Configuration.h
@@ -5,10 +5,24 @@
 #ifndef KOLMOGOROV_STEREO_CONFIGURATION_H
 #define KOLMOGOROV_STEREO_CONFIGURATION_H
 
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
 
 class Configuration {
+    Mat conf; // Cube matrix, shape = (w, h, maxDisp-minDisp+1)
+    Mat disparity;
+
+public:
+    Configuration(const Mat& conf); //constructor with explicit assignments
+    Configuration(const Mat& image, int minDisp, int maxDisp); //constructor with all inactive assignments, given an image and the disparity range
+    Mat computeDisparity();
+    bool validConfMatrix();
+
+    inline int operator()(int x, int y, int d) const { return conf.at<uchar>(y, x, d); } //Access the values of the assignment
 
 };
+
 
 
 #endif //KOLMOGOROV_STEREO_CONFIGURATION_H
