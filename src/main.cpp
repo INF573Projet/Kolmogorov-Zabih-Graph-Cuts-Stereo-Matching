3263366e82113960c60e3ee2f957288c6c95cebe
Leon Zheng <leonpapanoel@gmail.com>
2018-12-18 21:49:42 +0100

    Load image

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
-#include "image.h"
+
+#include "./src/image.h"
 
 using namespace cv;
 using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-    Image<Vec3b> img = Image<Vec3b> (imread("../images/right.png"));
-    cout << img << endl;
-    imshow("Input", img);
-    waitKey(0);
+    Mat mat = imread()
+    Image<uchar> img =
 
     return 0;
 }
